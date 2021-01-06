5aacd7fee6120c808e795d995e583738a1d1d479
daquintero <darioaquintero@gmail.com>
2021-01-05 23:24:08 -0500

    feat: Added license and restructured installation

--- README.md
+++ README.md
@@ -4,6 +4,24 @@ Straight up mix between NumPy, SymPy and Pandas into a single extendable framewo
 
 Future usage examples to follow.
 
+## Quick Start
+
+Local install after downloading the Anaconda distribution or equivalent:
+```
+>>> git clone https://github.com/daquintero/numpsy.git
+>>> cd numpsy
+>>> python3 setup.py install
+```
+
+## Quick Example
+```
+import numpsy as nsy
+
+
+```
+More advanced examples in `docs/examples/coplanar_waveguide_design.ipynb`.
+
+
 ### Is it any good?
 I think it's an elegant mathematical representation to simultanously perform symbolic, numerical, and data science operations into a single system.
 
--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
-import os
+import setuptools
 
-from distutils.core import setup
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-
-setup(name='numpsy',
-      version="0.0.0",
+setuptools.setup(name='numpsy',
+      version="0.0.1",
       description='NumpSy - Integrated NumPy, SymPy, Pandas and unit management for scientific programming.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/daquintero/numpsy",
       author='Dario Quintero',
       author_email='<darioaquintero> at gmail dot com',
-      url='',
-      packages=['numpsy'],
+      packages=setuptools.find_packages(),
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6',
      )
